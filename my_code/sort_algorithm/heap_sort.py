efef1f63f04321e06492b673e19b8a06eaaf9a5f
qiaokuahai <qiaokuahai@163.com>
2020-04-12 21:35:05 +0800

    修改：修改堆排序逻辑

--- my_code/sort_algorithm/heap_sort.py
+++ my_code/sort_algorithm/heap_sort.py
@@ -9,51 +9,56 @@ for i in range(0,length // 2 +1)[::-1]:
 第二部分是排序的T(n),因为HeapAdjust()是logn,一共执行n次，故T(n)=O(nlogn)
 因此总的时间复杂度为O(nlogn)
 
-例如[3, 5, 9, 8, 7]
+正序排序需要构造大顶堆，倒序构造小顶堆
 """
 
 
+# 本示例构造的是大顶堆
 def heap_sort(input_list):
     # 调整parent结点为大根堆
     def heap_adjust(input_list, parent, length):
-
-        temp = input_list[parent]
-        child = 2 * parent + 1
-
-        while child < length:
-            if child + 1 < length and input_list[child] < input_list[child + 1]:
-                child += 1
-
-            if temp > input_list[child]:
+        # 获取当前节点的左子树和右子树
+        parent_index = parent
+        left_child_index = parent_index * 2 + 1
+        right_child_index = parent_index * 2 + 2
+        curr_index = left_child_index  # 当前指针指向的位置，默认指向当前节点的左子树
+        while curr_index < length:
+            parent_value = input_list[parent_index]  # 首先获取父节点的值
+            if right_child_index < length:
+                if input_list[left_child_index] < input_list[right_child_index]:
+                    # 如果要构造小顶堆，1.需要改input_list[left_child_index] > input_list[right_child_index]
+                    # 如果要构造小顶堆，2.需要改parent_value < input_list[curr_index]
+                    curr_index += 1
+            # curr_value为当前节点的值，此时的curr_index已经是子节点中值最大的那个结点的index
+            if parent_value > input_list[curr_index]:
                 break
-            input_list[parent] = input_list[child]
-            parent = child
-            child = 2 * child + 1
-        input_list[parent] = temp
-    if input_list == []:
-        return []
-    sorted_list = input_list
-    length = len(sorted_list)
+            # 对调父节点和子节点的数据
+            input_list[parent_index], input_list[curr_index] = input_list[curr_index], input_list[parent_index]
+            parent_index = curr_index
+            left_child_index = parent_index * 2 + 1
+            right_child_index = parent_index * 2 + 2
+            curr_index = left_child_index
+
+    length = len(input_list)
     # 最后一个结点的下标为length//2-1
     # 建立初始大根堆
     # 构建堆的时候一定要倒着循环，这是因为底层的数据构建完成之后，上层的数据就可以根据底层的结果来构建。
     for i in range(0, length // 2)[::-1]:
-        heap_adjust(sorted_list, i, length)
+        heap_adjust(input_list, i, length)
 
     for j in range(1, length)[::-1]:
         # 把堆顶元素即第一大的元素与最后一个元素互换位置
-        temp = sorted_list[j]
-        sorted_list[j] = sorted_list[0]
-        sorted_list[0] = temp
+        input_list[0], input_list[j] = input_list[j], input_list[0]
         # 换完位置之后将剩余的元素重新调整成大根堆
-        heap_adjust(sorted_list, 0, j)
+        # 调整的时候就不用全部遍历了，单次深度往下走，将顶层元素放到合适的位置，因为除了顶层的元素， 其他的元素都满足根节点大于子节点
+        heap_adjust(input_list, 0, j)
         print('%dth' % (length - j))
-        print(sorted_list)
-    return sorted_list
+        print(input_list)
+    return input_list
 
 
 if __name__ == '__main__':
-    input_list = [50, 123, 543, 187, 49, 30, 0, 2, 11, 100, 1, 6, 249]
+    input_list = [50, 123, 543, 187, 8, 9, 7, 49, 30, 0, 2, 11, 100, 1, 6, 249]
     print("input_list:")
     print(input_list)
     sorted_list = heap_sort(input_list)
